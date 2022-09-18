4128b225c85f4cba49d496355f8b55cb9fe59231
Gargi Chakraverty <gchakraverty@MacBook-Pro.local>
2022-09-18 21:06:02 +0530

    adding some utility functions

---
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/BindingFunctions/PrimaryStringGen.java
@@ -0,0 +1,31 @@
+package com.oltpbenchmark.benchmarks.featurebench.BindingFunctions;
+
+public class PrimaryStringGen {
+    private int currentValue;
+    private int desiredLength;
+    private String key;
+
+    public PrimaryStringGen(int startNumber, int desiredLength) {
+        this.currentValue = startNumber - 1;
+        this.desiredLength = desiredLength;
+    }
+
+    public String getValue() {
+        return key;
+    }
+
+    public String getNextValue() {
+        currentValue++;
+        key = numberToIdString(currentValue);
+        return key;
+    }
+
+    public String numberToIdString(int number) {
+        StringBuilder baseNumberStr = new StringBuilder(String.valueOf(currentValue));
+        while (baseNumberStr.length() < desiredLength) {
+            baseNumberStr.append('a');
+        }
+        return baseNumberStr.toString();
+    }
+
+}
