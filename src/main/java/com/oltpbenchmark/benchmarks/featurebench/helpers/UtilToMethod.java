bce28169eb7e564d2d3c48e54a397338a6651d27
Rakshit Jain <54314936+rakshitjain13@users.noreply.github.com>
2022-10-03 13:11:55 +0530

    added normal and exponential distribution (#6)
    * added normal and exponential distribution
    
    * format

--- src/main/java/com/oltpbenchmark/benchmarks/featurebench/helpers/UtilToMethod.java
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/helpers/UtilToMethod.java
@@ -20,9 +20,12 @@ public class UtilToMethod {
                 params = new ArrayList<>(List.of());
             }
             this.clsInstance = (BaseUtil) cls.getDeclaredConstructor(List.class).newInstance((List) params);
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException |
-                 IllegalAccessException e) {
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
             throw new RuntimeException(String.format("Oops! Are you sure that you provided the right utility function name: %s ? ", util));
+        } catch (InvocationTargetException | InstantiationException |
+                 IllegalAccessException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
         }
 
     }
