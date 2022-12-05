0b3e5437c6811fa934d0a06ab0c48baea3443d81
Rakshit Jain <rjain@yugabyte.com>
2022-12-05 11:39:35 +0530

    fix

--- src/main/java/com/oltpbenchmark/benchmarks/featurebench/customworkload/CreateStatement.java
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/customworkload/CreateStatement.java
@@ -5,18 +5,25 @@ import com.oltpbenchmark.benchmarks.featurebench.YBMicroBenchmark;
 import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.apache.commons.configuration2.tree.ImmutableNode;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CreateStatement extends YBMicroBenchmark {
     public CreateStatement(HierarchicalConfiguration<ImmutableNode> config) {
         super(config);
         this.executeOnceImplemented = true;
     }
+
     public void executeOnce(Connection conn, BenchmarkModule benchmarkModule) throws SQLException {
         int count = 100;
         for (int i = 0; i < count; i++) {
             Statement stmt = conn.createStatement();
-            stmt.execute("select * from pkey_hash_point_lookup_tbl_128_par_1 where col_varchar_id_1='1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';");
+            ResultSet rs = stmt.executeQuery("select * from pkey_hash_point_lookup_tbl_128_par_1 where col_varchar_id_1='1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
         }
     }
 }
--- src/main/java/com/oltpbenchmark/benchmarks/featurebench/customworkload/NewConnection.java
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/customworkload/NewConnection.java
@@ -5,19 +5,26 @@ import com.oltpbenchmark.benchmarks.featurebench.YBMicroBenchmark;
 import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.apache.commons.configuration2.tree.ImmutableNode;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class NewConnection extends YBMicroBenchmark {
     public NewConnection(HierarchicalConfiguration<ImmutableNode> config) {
         super(config);
         this.executeOnceImplemented = true;
     }
+
     public void executeOnce(Connection conn, BenchmarkModule benchmarkModule) throws SQLException {
         int count = 100;
         for (int i = 0; i < count; i++) {
             Connection newconn = benchmarkModule.makeConnection();
             Statement stmt = newconn.createStatement();
-            stmt.execute("select * from pkey_hash_point_lookup_tbl_128_par_1 where col_varchar_id_1='1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';");
+            ResultSet rs = stmt.executeQuery("select * from pkey_hash_point_lookup_tbl_128_par_1 where col_varchar_id_1='1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
         }
     }
 }
