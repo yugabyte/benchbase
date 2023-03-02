90a243c445b8a05799741f6b0825d09541479810
shaharuk-yb <sshaikh@yugabyte.com>
2023-03-02 19:57:38 +0530

    fix

--- src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/PrimaryIntGen.java
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/PrimaryIntGen.java
@@ -1,9 +1,6 @@
 package com.oltpbenchmark.benchmarks.featurebench.utils;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
 
 
@@ -41,15 +38,11 @@ public class PrimaryIntGen implements BaseUtil {
             throw new RuntimeException("Incorrect number of parameters for util function "
                 + this.getClass());
         }
-
-        int low = (int) values.get(0);
-        int high = (int) values.get(1);
-
-        int diff = (high - low) + 1 == high ? Math.floorDiv(high, totalWorkers) : Math.floorDiv((high-low), totalWorkers);
-
-        this.lowerRange = workerId == 0 ? low : low + workerId * diff + 1;
-        this.upperRange = Math.min(low + diff* (workerId+1), high);
-        this.currentValue = this.lowerRange;
+        int divide = (((Number) values.get(1)).intValue() - ((Number) values.get(0)).intValue()) / totalWorkers;
+        this.currentValue = ((Number) values.get(0)).intValue() - 1 + divide * workerId;
+        int upperRangeTemp = (((Number) values.get(0)).intValue() + (divide) * (workerId + 1) + (workerId == 0 ? 0 : 1));
+        this.upperRange = Math.min(upperRangeTemp, ((Number) values.get(1)).intValue());
+        this.lowerRange = ((Number) values.get(0)).intValue() + divide * (workerId) + (workerId == 0 ? 0 : 1);
         if (upperRange < lowerRange) {
             throw new RuntimeException("Upper bound less than lower bound");
         }
@@ -63,7 +56,7 @@ public class PrimaryIntGen implements BaseUtil {
 
     @Override
     public Object run() {
-        if (currentValue > upperRange) {
+        if (currentValue >= upperRange) {
             throw new RuntimeException("Out of bounds primary key access");
         }
         currentValue = findNextHigherValue();
