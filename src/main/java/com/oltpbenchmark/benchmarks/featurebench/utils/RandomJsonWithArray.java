6c19b6d927f7551edbfaca4e9cca811f87acd27e
Hemant Bhanawat <hbhanawat@yugabyte.com>
2023-03-06 11:52:14 +0530

    random json

--- src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/RandomJsonWithArray.java
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/RandomJsonWithArray.java
@@ -1,40 +1,42 @@
 package com.oltpbenchmark.benchmarks.featurebench.utils;
 
-
 import org.json.JSONObject;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class RandomJson implements BaseUtil {
-
+public class RandomJsonWithArray implements BaseUtil {
     protected int fields;
-    protected int valueLength;
-    protected int nestedness;
-    protected RandomStringAlphabets  randomAlphabets;
-    public RandomJson(List<Object> values) {
+    protected int arrayLength;
+    protected PrimaryStringGen  randomAlphabets;
+
+    public RandomJsonWithArray(List<Object> values) {
         if (values.size() < 2) {
             throw new RuntimeException("Incorrect number of parameters for util function "
                 + this.getClass());
         }
         this.fields = (int)values.get(0);
-        this.valueLength = (int)values.get(1);
-        if (values.size() > 2)
-            this.nestedness = (int)values.get(2);
-        randomAlphabets = new RandomStringAlphabets(Collections.singletonList(valueLength));
-    }
+        this.arrayLength = (int)values.get(1);
+        ArrayList<Object> params = new ArrayList<>();
+        params.add(0);
+        params.add(20);
+        randomAlphabets = new PrimaryStringGen(params);
 
-    public RandomJson(List<Object> values, int workerId, int totalWorkers) {
-        this(values);
     }
 
     @Override
     public Object run() throws ClassNotFoundException, InvocationTargetException,
         NoSuchMethodException, InstantiationException, IllegalAccessException {
         JSONObject outer = new JSONObject();
+
         for (int i = 0; i < fields; i++) {
-            outer.put(Integer.toString(i), randomAlphabets.run());
+            ArrayList<String> arrValues = new ArrayList<>();
+            for (int j = 0; j < arrayLength; j++) {
+                arrValues.add(randomAlphabets.run().toString());
+            }
+            outer.put(Integer.toString(i), arrValues);
         }
         return outer.toString();
     }
