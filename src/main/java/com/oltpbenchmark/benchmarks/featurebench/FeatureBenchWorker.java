9f501e049eae3d9a57b509c0433e38b769fce7bd
shaharuk-yb <sshaikh@yugabyte.com>
2024-11-18 15:59:36 +0530

    added if clause

--- src/main/java/com/oltpbenchmark/benchmarks/featurebench/FeatureBenchWorker.java
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/FeatureBenchWorker.java
@@ -331,7 +331,8 @@ public class FeatureBenchWorker extends Worker<FeatureBenchBenchmark> {
 
 //                    System.out.printf("Explain plan RC %s%n", queryToExplainMap.getOrDefault(entry.getKey(), new JSONObject()).get("ExplainPlanRows"));
 //                    System.out.printf("user provided RC %d%n", entry.getValue());
-                    inner.put("explainPlanRcValidationSuccess", Integer.parseInt((String) queryToExplainMap.getOrDefault(entry.getKey(), new JSONObject()).get("ExplainPlanRows")) == entry.getValue());
+                    if (entry.getValue() != -1)
+                        inner.put("explainPlanRcValidationSuccess", Integer.parseInt((String) queryToExplainMap.getOrDefault(entry.getKey(), new JSONObject()).get("ExplainPlanRows")) == entry.getValue());
                     inner.put("explain", queryToExplainMap.getOrDefault(entry.getKey(), new JSONObject()));
                     /*TODO: remove prepared_statements*/
                     inner.put("prepared_statements", pgPreparedStatementOutputs == null ? new JSONObject() : pgPreparedStatementOutputs);
