6e8be0b9a9335b7fe6dc590b7a779f9753482cff
Yash Tyagi <yashtyagis9578@gmail.com>
2025-04-14 13:51:52 +0530

    added functions for sequential date generation

---
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/CyclicSeqDateGen.java
@@ -0,0 +1,53 @@
+package com.oltpbenchmark.benchmarks.featurebench.utils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDate;
+import java.util.List;
+
+public class CyclicSeqDateGen implements BaseUtil {
+
+    private final int yearLowerBound;
+    private final int yearUpperBound;
+    private LocalDate currentDate;
+
+
+    public CyclicSeqDateGen(List<Object> values) {
+        if (values.size() != 2) {
+            throw new RuntimeException("Incorrect number of parameters for util function "
+                + this.getClass());
+        }
+        this.yearLowerBound = ((Number) values.get(0)).intValue();
+        this.yearUpperBound = ((Number) values.get(1)).intValue();
+        if (yearLowerBound > yearUpperBound) {
+            throw new RuntimeException("Please enter correct values for yearLowerBound and yearUpperBound");
+        }
+        this.currentDate =  LocalDate.of(yearLowerBound, 1, 1);
+    }
+
+    public CyclicSeqDateGen(List<Object> values,int workerId,int totalWorkers) {
+        if (values.size() != 2) {
+            throw new RuntimeException("Incorrect number of parameters for util function "
+                + this.getClass());
+        }
+        this.yearLowerBound = ((Number) values.get(0)).intValue();
+        this.yearUpperBound = ((Number) values.get(1)).intValue();
+        if (yearLowerBound > yearUpperBound) {
+            throw new RuntimeException("Please enter correct values for yearLowerBound and yearUpperBound");
+        }
+        this.currentDate =  LocalDate.of(yearLowerBound, 1, 1);
+    }
+
+    private LocalDate findNextHigherValue() {
+        return currentDate.plusDays(1);
+    }
+
+    @Override
+    public Object run() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        currentDate=findNextHigherValue();
+        if(currentDate.getYear()>yearUpperBound)
+        {
+            currentDate=LocalDate.of(yearLowerBound, 1, 1);
+        }
+        return currentDate;
+    }
+}
---
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/CyclicSeqDateGenWithRep.java
@@ -0,0 +1,60 @@
+package com.oltpbenchmark.benchmarks.featurebench.utils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDate;
+import java.util.List;
+
+public class CyclicSeqDateGenWithRep implements BaseUtil {
+
+    private final int yearLowerBound;
+    private final int yearUpperBound;
+    private final int maxDays;
+    private int dayCounter=0;
+    private LocalDate currentDate;
+
+
+    public CyclicSeqDateGenWithRep(List<Object> values) {
+        if (values.size() != 3) {
+            throw new RuntimeException("Incorrect number of parameters for util function "
+                + this.getClass());
+        }
+        this.yearLowerBound = ((Number) values.get(0)).intValue();
+        this.yearUpperBound = ((Number) values.get(1)).intValue();
+        this.maxDays = ((Number) values.get(2)).intValue();
+        if (yearLowerBound > yearUpperBound) {
+            throw new RuntimeException("Please enter correct values for yearLowerBound and yearUpperBound");
+        }
+        this.currentDate =  LocalDate.of(yearLowerBound, 1, 1);
+    }
+
+    public CyclicSeqDateGenWithRep(List<Object> values,int workerId,int totalWorkers) {
+        if (values.size() != 3) {
+            throw new RuntimeException("Incorrect number of parameters for util function "
+                + this.getClass());
+        }
+        this.yearLowerBound = ((Number) values.get(0)).intValue();
+        this.yearUpperBound = ((Number) values.get(1)).intValue();
+        this.maxDays = ((Number) values.get(2)).intValue();
+        if (yearLowerBound > yearUpperBound) {
+            throw new RuntimeException("Please enter correct values for yearLowerBound and yearUpperBound");
+        }
+        this.currentDate =  LocalDate.of(yearLowerBound, 1, 1);
+    }
+
+    private LocalDate findNextHigherValue() {
+        dayCounter++;
+        return currentDate.plusDays(1);
+    }
+
+    @Override
+    public Object run() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        currentDate=findNextHigherValue();
+        if(currentDate.getYear()>yearUpperBound || dayCounter>maxDays)
+        {
+            currentDate=LocalDate.of(yearLowerBound, 1, 1);
+            if(dayCounter>maxDays) 
+            dayCounter=1;
+        }
+        return currentDate;
+    }
+}
