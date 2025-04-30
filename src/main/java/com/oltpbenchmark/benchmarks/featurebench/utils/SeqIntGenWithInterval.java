f184b10e9caba5dc8fde24b4ba57d76e74d9ee07
Yash Tyagi <yashtyagis9578@gmail.com>
2025-04-30 11:06:47 +0530

    minor corrections

---
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/SeqIntGenWithInterval.java
@@ -0,0 +1,62 @@
+package com.oltpbenchmark.benchmarks.featurebench.utils;
+
+
+import java.util.List;
+
+
+public class SeqIntGenWithInterval implements BaseUtil {
+    private final int upperRange;
+    private final int lowerRange;
+    private final int interval;
+    private int currentValue, counter=0;
+
+    public SeqIntGenWithInterval(List<Object> values) {
+        if (values.size() != 3) {
+            throw new RuntimeException("Incorrect number of parameters for util function "
+                + this.getClass());
+        }
+        this.currentValue = ((Number) values.get(0)).intValue();
+        this.upperRange = ((Number) values.get(1)).intValue();
+        this.lowerRange = ((Number) values.get(0)).intValue();
+        this.interval = ((Number) values.get(2)).intValue();
+        if (upperRange < lowerRange) {
+            throw new RuntimeException("Upper bound less than lower bound");
+        }
+    }
+
+    public SeqIntGenWithInterval(List<Object> values, int workerId, int totalWorkers) {
+        if (values.size() != 3) {
+            throw new RuntimeException("Incorrect number of parameters for util function "
+                + this.getClass());
+        }
+        int divide = (((Number) values.get(1)).intValue() - ((Number) values.get(0)).intValue()) / totalWorkers;
+        this.currentValue = ((Number) values.get(0)).intValue() + divide * workerId;
+        int upperRangeTemp = (((Number) values.get(0)).intValue() + (divide) * (workerId + 1) + (workerId == 0 ? 0 : 1));
+        this.upperRange = Math.min(upperRangeTemp, ((Number) values.get(1)).intValue());
+        this.lowerRange = ((Number) values.get(0)).intValue() + divide * (workerId) + (workerId == 0 ? 0 : 1);
+        this.interval = ((Number) values.get(2)).intValue();
+        if (upperRange < lowerRange) {
+            throw new RuntimeException("Upper bound less than lower bound");
+        }
+
+    }
+
+    private int findNextHigherValue() {
+        counter++;
+        if(counter==interval)
+        {
+            currentValue++;
+            counter=0;
+        }
+        return currentValue;
+    }
+
+    @Override
+    public Object run() {
+        if (currentValue >= upperRange) {
+            throw new RuntimeException("Out of bounds key access");
+        }
+        currentValue = findNextHigherValue();
+        return currentValue;
+    }
+}
