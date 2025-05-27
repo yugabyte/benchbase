207aa54f7250da376cdb4538229ecce1939f9632
Rakshit Jain <rakshitdhoka13@gmail.com>
2025-05-27 11:15:46 +0530

    adding load constructuor

--- src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/PrimaryIntRandomForExecutePhase.java
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/utils/PrimaryIntRandomForExecutePhase.java
@@ -10,7 +10,24 @@ public class PrimaryIntRandomForExecutePhase implements BaseUtil {
     private static final Logger LOG = LoggerFactory.getLogger(PrimaryIntGen.class);
     private final int upperRange;
     private final int lowerRange;
-    private int currentValue;
+
+
+     public PrimaryIntRandomForExecutePhase(List<Object> values) {
+        if (values.size() != 2) {
+            throw new RuntimeException("Incorrect number of parameters for util function "
+                + this.getClass());
+        }
+
+        int low = (int) values.get(0);
+        int high = (int) values.get(1);
+
+        this.lowerRange = low;
+        this.upperRange = high;
+        if (upperRange < lowerRange) {
+            throw new RuntimeException("Upper bound less than lower bound");
+        }
+
+    }
 
     public PrimaryIntRandomForExecutePhase(List<Object> values, int workerId, int totalWorkers) {
         if (values.size() != 2) {
@@ -25,7 +42,6 @@ public class PrimaryIntRandomForExecutePhase implements BaseUtil {
 
         this.lowerRange = workerId == 0 ? low : low + workerId * diff + 1;
         this.upperRange = Math.min(low + diff * (workerId + 1), high);
-        this.currentValue = this.lowerRange;
         if (upperRange < lowerRange) {
             throw new RuntimeException("Upper bound less than lower bound");
         }
